d792f2dfd69b132b1e929078061393dddaea4093
Simon Maribo <simonmaribo@outlook.dk>
2022-09-17 21:01:16 +0200

    caching

--- server/api/lastdeath.ts
+++ server/api/lastdeath.ts
@@ -4,13 +4,12 @@ const redis = new Redis(process.env.REDIS_URL);
 
 export default defineEventHandler(async (event) => {
 
-
     const name = await redis.get("plexit-ldv-name");
     const timestamp = await redis.get("plexit-ldv-timestamp");
 
     event.res.setHeader("Content-Type", "application/json");
     event.res.setHeader("Access-Control-Allow-Origin", "*");
-    event.res.setHeader("Cache-Control", "s-maxage=60, stale-while-revalidate");
+    event.res.setHeader("Cache-Control", "max-age=60, public");
 
 
     return {
