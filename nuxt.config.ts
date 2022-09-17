659773eb59d241430d6ef5718da9bf6d740a3f98
Simon Maribo <simonmaribo@outlook.dk>
2022-09-17 18:59:28 +0200

    fixed shit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    meta: {
+        title: 'Seneste døde vagt',
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico', id: "favicon"},
+        ]
+    }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,13 @@
     <div class="main">
         <h1>Seneste døde vagt</h1>
         <div class="section">
-            <img :src="'https://cravatar.eu/head/' + data?.name"/>
-            <h1 class="name">{{data?.name || "Loading..."}}</h1>
+            <img :src="'https://cravatar.eu/head/' + user"/>
+            <h1 class="name">{{user || "Loading..."}}</h1>
             <div class="time">
-                <TimeUnit v-if="days > 0" unit="dage" :time="days" :size="200" color="#db4844"/>
-                <TimeUnit v-if="hours > 0" unit="timer" :time="hours" :size="160" color="#f07c22"/>
-                <TimeUnit v-if="minutes > 0" unit="minutter" :time="minutes" :size="120" color="#f6da74"/>
-                <TimeUnit v-if="seconds > 0" unit="sekunder" :time="seconds" :size="70" color="#abcd58"/>
+                <TimeUnit v-if="days > 0" :unit="days == 1 ? 'dag' : 'dage'" :time="days" :size="200" color="#db4844"/>
+                <TimeUnit v-if="hours > 0" :unit="hours == 1 ? 'time' : 'timer'" :time="hours" :size="160" color="#f07c22"/>
+                <TimeUnit v-if="minutes > 0" :unit="minutes == 1 ? 'minut' : 'minutter'" :time="minutes" :size="120" color="#f6da74"/>
+                <TimeUnit v-if="seconds > 0" :unit="seconds == 1 ? 'sekund' : 'sekunder'" :time="seconds" :size="70" color="#abcd58"/>
             </div>
         </div>
     </div>
@@ -59,7 +59,6 @@
 </style>
 
 <script lang="ts">
-
 import TimeUnit from '~~/components/TimeUnit.vue';
 
 export default {
@@ -69,22 +68,20 @@ export default {
     },
     data() {
         return {
+            user: null,
+            timestamp: null,
             seconds: 0,
             minutes: 0,
             hours: 0,
             days: 0,
         }
     },
-    setup() {
-        const { data } = useFetch(() => '/api/lastdeath');
-        return { data };
-    },
     methods: {
         update() {
             var days = 0, hours = 0, minutes = 0, seconds = 0;
-            if(!this.data.timestamp) return;
+            if(!this.timestamp || this.timestamp == null) return;
 
-            var diff = Math.floor((new Date().getTime() / 1000) - this.data.timestamp);
+            var diff = Math.floor((new Date().getTime() / 1000) - this.timestamp);
 
             days = Math.floor(diff / 86400);
             hours = Math.floor(diff / 3600) - ( days * 24 );
@@ -95,17 +92,24 @@ export default {
             this.minutes = minutes;
             this.hours = hours;
             this.days = days;
+        },
+        updateMeta() {
+            document.title = this.user + " | Seneste døde vagt";
+            document.getElementById("favicon").href = "https://cravatar.eu/helmhead/" + this.user + "/64.png";
+        },
+        fetchData(){
+            $fetch('/api/lastdeath').then((data) => {
+                this.user = data.name;
+                this.timestamp = data.timestamp;
+                this.updateMeta();
+            });
         }
     },
-    created() {
+    async mounted() {
+        this.fetchData();
         setInterval(() => {
             this.update();
         }, 1000);
     },
-    head() {
-        return {
-            title: this.data?.name || "Loading...",
-        }
-    }
 }
 </script>
